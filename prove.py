57f749e40c870345259e176d9e19bc126a6e83ad
Dimitry Krakitov <dimitry@localhost.localdomain>
2017-12-01 22:42:49 +0000

    delete this Nephew

--- prove.py
+++ prove.py
@@ -30,16 +30,16 @@ for angola in independencia[1:-1]:
 portugal = independencia.copy()
 while portugal:
     for item in args:#cycle through arguments we know are true, till we find one still in unused sentences
-        for elem in portugal:
-            if negation(item) in elem:#if a negated element is found in the disjunction, then the other side is true and added to the "knowledge"
-                another = elem #the sentence
+        for thisNephew in portugal:
+            if negation(item) in thisNephew:#if a negated element is found in the disjunction, then the other side is true and added to the "knowledge"
+                another = thisNephew #the sentence
                 another.remove(negation(item))#choose the other side of the disjunction, the "new truth"
                 if negation(another[0]) in args: #if the negation of this truth was already in "the knowledge", we have a contradiction, and we prove the resolution "True"
                     print("True")
                     exit()
                 if another[0] not in args:
                     args.append(another)#if it's a new Truth, add to the "knowledge"
-                portugal.remove(elem)
+                portugal.remove(thisNephew)
                 break
 #talvez seja possivel que no fim fiquem só sentences perdidas no portugal que n "provam" nada e nunca foram usadas e assim
 #  ficamos num ciclo infinito
