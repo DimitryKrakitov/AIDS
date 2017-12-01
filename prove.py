be27fbdd2edf113096fa5ead62a4b316df833dd1
Dimitry Krakitov <dimitry@localhost.localdomain>
2017-12-01 22:54:25 +0000

    anotha one

--- prove.py
+++ prove.py
@@ -32,13 +32,13 @@ while portugal:
     for Nigga in Jail:#cycle through arguments we know are true, till we find one still in unused sentences
         for thisNephew in portugal:
             if negation(Nigga) in thisNephew:#if a negated element is found in the disjunction, then the other side is true and added to the "knowledge"
-                another = thisNephew #the sentence
-                another.remove(negation(Nigga))#choose the other side of the disjunction, the "new truth"
-                if negation(another[0]) in Jail: #if the negation of this truth was already in "the knowledge", we have a contradiction, and we prove the resolution "True"
+                anotha_one = thisNephew #the sentence
+                anotha_one.remove(negation(Nigga))#choose the other side of the disjunction, the "new truth"
+                if negation(anotha_one[0]) in Jail: #if the negation of this truth was already in "the knowledge", we have a contradiction, and we prove the resolution "True"
                     print("True")
                     exit()
-                if another[0] not in Jail:
-                    Jail.append(another)#if it's a new Truth, add to the "knowledge"
+                if anotha_one[0] not in Jail:
+                    Jail.append(anotha_one)#if it's a new Truth, add to the "knowledge"
                 portugal.remove(thisNephew)
                 break
 #talvez seja possivel que no fim fiquem só sentences perdidas no portugal que n "provam" nada e nunca foram usadas e assim
