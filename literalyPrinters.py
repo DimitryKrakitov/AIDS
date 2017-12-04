be35a0ad66bd796652410eced62d6e2a1ce9ccb1
Dimitry Krakitov <dimitry@localhost.localdomain>
2017-12-04 19:13:12 +0000

    prove.py faz mal comparações: 'string A in B' não está a funcionar

---
+++ literalyPrinters.py
@@ -0,0 +1,7 @@
+
+
+def printer(alameda):
+    surprise = "["
+    for Quenga in alameda:
+        surprise += " " + str(Quenga) + ";"
+    return surprise + "]"
--- prove.py
+++ prove.py
@@ -1,7 +1,7 @@
 from filereader import *
 from convFunctions import negation
 import sys
-
+from literalyPrinters import *
 
 if len(sys.argv) != 2:
     print("Not the right amount of arguments.")
@@ -20,20 +20,29 @@ Jail.append(independencia[-1])
 portugal = []
 #portugal.extend(x for x in independencia if len(x) > 1)
 
-for angola in independencia[1:-1]:
+for angola in independencia[0:-1]:
     if len(angola) == 1:#if sentence is just literal, add to arguments
         Jail.append(angola)
     else:
         portugal.append(angola)#if not literal (sentence) add to portugal
 
 
-portugal = independencia.copy()
+#portugal = independencia.copy()
+print("Portugal in the Begining: " + str(portugal))
 #while portugal:
 for Nigga in Jail:#cycle through arguments we know are true, till we find one still in unused sentences
+    print("")
+    print("     Jail is now: " + str(Jail))
+    #print(printer(Jail))
     print("Who's this Nigga?")
     print(Nigga)
+    print("  Portugal is now: " + str(portugal))
+    #print(printer(Jail))
     for thisNephew in portugal:
+        print("thisNephew is :" + str(thisNephew))
+        print("is " + negation(str(Nigga)) + " in " + str(thisNephew))
         if negation(str(Nigga)) in thisNephew:#if a negated element is found in the disjunction, then the other side is true and added to the "knowledge"
+            print("Yes my dudes")
             anotha_one = thisNephew #the sentence
             anotha_one.remove(negation(Nigga))#choose the other side of the disjunction, the "new truth"
             if negation(anotha_one[0]) in Jail: #if the negation of this truth was already in "the knowledge", we have a contradiction, and we prove the resolution "True"
@@ -43,6 +52,8 @@ for Nigga in Jail:#cycle through arguments we know are true, till we find one st
                 Jail.append(anotha_one)#if it's a new Truth, add to the "knowledge"
             portugal.remove(thisNephew)
             break
+        else:
+            print("Nope my dudes")
 #talvez seja possivel que no fim fiquem só sentences perdidas no portugal que n "provam" nada e nunca foram usadas e assim
 #  ficamos num ciclo infinito
 print("False")
